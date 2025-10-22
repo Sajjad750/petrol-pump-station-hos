7b8a94e744003f89a5980d956bd1af35ff0ed379
Hassan <hassanfayyaz19@gmail.com>
2025-10-22 07:25:51 +0500

    tweaks

--- database/migrations/2025_10_22_010049_create_shift_pump_totals_table.php
+++ database/migrations/2025_10_22_010049_create_shift_pump_totals_table.php
@@ -15,7 +15,7 @@ return new class () extends Migration {
             $table->uuid('uuid')->nullable()->unique();
 
             $table->foreignId('station_id')->constrained()->onDelete('cascade');
-            $table->foreign('shift_id');
+            $table->unsignedBigInteger('shift_id');
             $table->integer('pump_id');
             $table->integer('nozzle_id');
             $table->integer('fuel_grade_id');
