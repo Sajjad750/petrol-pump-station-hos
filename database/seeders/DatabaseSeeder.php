fe2a69bfc6f031e730944e7cc59bed3afeb86218
Hassan <hassanfayyaz19@gmail.com>
2025-10-23 10:37:29 +0500

    removed dummy seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             GenerateApiKeyForTestSiteSeeder::class,
-            FuelGradeSeeder::class,
+//            FuelGradeSeeder::class,
         ]);
     }
 }
